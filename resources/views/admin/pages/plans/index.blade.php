855fda9d36e22e4ec9c797b29a39a4a712152b5d
Rogerio Pereira <rogerio@colmeiatecnologia.com.br>
2020-12-02 10:02:18 -0500

    Breadcrumb

--- resources/views/admin/pages/plans/index.blade.php
+++ resources/views/admin/pages/plans/index.blade.php
@@ -3,6 +3,11 @@
 @section('title', 'Planos')
 
 @section('content_header')
+    <ol class="breadcrumb">
+        <li class='breadcrumb-item'><a href='{{route('admin.index')}}'>Dashboard</a></li>
+        <li class='breadcrumb-item active'><a href='{{route('admin.plans.index')}}'>Planos</a></li>
+    </ol>
+
     <h1>
         Planos 
         <a href='{{route('admin.plans.create')}}' class='btn btn-dark'>ADD</a>
